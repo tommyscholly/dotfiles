0c4880074418f5bee8e363414ba9c6df3e5b8ffd
Tom Schollenberger <tss2344@rit.edu>
2025-03-05 21:08:38 -0500

    sync neovim conf

--- neovim/nvim/lua/plugins/ai.lua
+++ neovim/nvim/lua/plugins/ai.lua
@@ -5,25 +5,33 @@ return {
    --       require("supermaven-nvim").setup({})
    --    end,
    -- },
-   -- "github/copilot.vim",
    {
-      "zbirenbaum/copilot.lua",
+      "github/copilot.vim",
       config = function()
          -- Disable Copilot for all filetypes, then enable it for Python.
          vim.g.copilot_filetypes = {
             java = false,
          }
-
-         require("copilot").setup({
-            suggestion = { enabled = false },
-            panel = { enabled = false },
-         })
-      end,
-   },
-   {
-      "zbirenbaum/copilot-cmp",
-      config = function()
-         require("copilot_cmp").setup()
       end
    },
+   -- {
+   --    "zbirenbaum/copilot.lua",
+   --    config = function()
+   --       -- Disable Copilot for all filetypes, then enable it for Python.
+   --       vim.g.copilot_filetypes = {
+   --          java = false,
+   --       }
+   --
+   --       require("copilot").setup({
+   --          suggestion = { enabled = true },
+   --          panel = { enabled = false },
+   --       })
+   --    end,
+   -- },
+   -- {
+   --    "zbirenbaum/copilot-cmp",
+   --    config = function()
+   --       require("copilot_cmp").setup()
+   --    end
+   -- },
 }
